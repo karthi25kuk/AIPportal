80c56620bb427b53f7b4ae09538be9f521977750
karthi25kuk <karthikeyankannan.vnr@gmail.com>
2026-03-25 10:15:42 +0530

    fixed axios

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,10 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // Add a request interceptor to add the token
