014c3a84c5a4042f6c4d8397186c9985d9d3177a
karthi25kuk <karthikeyankannan.vnr@gmail.com>
2026-03-26 14:40:11 +0530

    function change AR - 2

--- client/src/pages/Admindashboard.jsx
+++ client/src/pages/Admindashboard.jsx
@@ -104,7 +104,7 @@ export default function AdminDashboard() {
 
   const handleLogout = () => {
     localStorage.clear();
-    navigate("/api/adminlogin");
+    navigate("/adminlogin");
   };
 
   return (
